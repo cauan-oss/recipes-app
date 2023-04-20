1a689fe37405dc6dc537c12a7e22bd465a10875e
Lílian Vieira <lilianv528@gmail.com>
2023-04-20 19:09:04 -0300

    req 6

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -5,23 +5,27 @@ import App from '../App';
 import ProviderLogin from '../context/ContextLogin ';
 import renderWithRouter from '../renderWithRouter';
 
-describe('app de receitas', () => {
-  test('Login', () => {
+describe('Testando tela de login', () => {
+  it('Login', () => {
     renderWithRouter(
       <ProviderLogin>
         <App />
       </ProviderLogin>,
     );
 
-    const inputEmail = screen.getByTestId('email-input');
-    const inputPassword = screen.getByTestId('password-input');
-    const btnSubmit = screen.getByTestId('login-submit-btn');
-    expect(inputEmail).toBeInTheDocument();
-    expect(inputPassword).toBeInTheDocument();
-    expect(btnSubmit).toBeDisabled();
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const buttonLogin = screen.getByTestId('login-submit-btn');
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(buttonLogin).toBeInTheDocument();
+    userEvent.type(emailInput, 'testeEmail');
+    userEvent.type(passwordInput, '222');
+    expect(buttonLogin).toBeDisabled();
 
-    userEvent.type(inputEmail, 'teste@teste.com');
-    userEvent.type(inputPassword, '000000');
-    expect(btnSubmit).not.toBeDisabled();
+    userEvent.type(emailInput, 'teste@teste.com');
+    userEvent.type(passwordInput, '12345678');
+    expect(buttonLogin).toBeEnabled();
+    userEvent.click(buttonLogin);
   });
 });
